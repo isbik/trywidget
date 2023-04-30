a39008a5fcce8c36d1c7c072ec279aadee5551ec
Barashov <barashovmisha@gmail.com>
2023-04-30 11:57:32 +0300

    удаление неактивных файлов

--- backend/settings/celery.py
+++ backend/settings/celery.py
@@ -1,2 +1,15 @@
-CELERY_BROKER_URL = "redis://redis:6381"
-CELERY_RESULT_BACKEND = "redis://redis:6381"
+from celery.schedules import crontab
+from datetime import timedelta
+
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6381"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6381"
+
+CELERY_BEAT_SCHEDULE = {
+    'delete_inactive_files': {
+        'task': 'apps.files.tasks.delete_inactive_files',
+        'schedule': crontab(minute='0', hour='*/2')
+    }
+}
+
+DELETE_INACTIVE_FILES_TIME = timedelta(hours=1)
