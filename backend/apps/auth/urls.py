6bc6969fe4eee432d3a7cc217a9d40b36a853ca2
Barashov <barashovmisha@gmail.com>
2023-05-03 19:21:39 +0300

    register

--- backend/apps/auth/urls.py
+++ backend/apps/auth/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
+    path('email/verify/<token>/', views.verify_email_view)
 ]
--- backend/apps/users/services/email.py
+++ backend/apps/users/services/email.py
@@ -3,3 +3,10 @@ from django.contrib.auth import get_user_model
 
 def is_email_taken(email: str):
     return get_user_model().objects.filter(email=email).exists()
+
+
+def get_user_by_verify_token(token: str):
+    try:
+        return get_user_model().objects.get(email_verify_token=token)
+    except:
+        return
