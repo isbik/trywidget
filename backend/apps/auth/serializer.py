6624bd49528a54857daf6896e1308c0ca9528ae0
Barashov <barashovmisha@gmail.com>
2023-05-06 19:05:32 +0300

    правки в отправке email при регистрации

--- backend/apps/auth/serializer.py
+++ backend/apps/auth/serializer.py
@@ -1,11 +1,14 @@
 from rest_framework.serializers import ValidationError
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from apps.users.services.email import is_email_taken
-from apps.emails.tasks import send_email
 from uuid import uuid4
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
+
+import settings
+from apps.users.services.email import is_email_taken
+from apps.emails.tasks import send_email
+from apps.emails.services.token import send_token
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -31,16 +34,22 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(data['password'])
         user.save()
 
-        html_message = render_to_string(
-            'emails/register.html', {'token': verify_token})
+        send_token(user.email,
+                   'Register',
+                   'register.html',
+                   client_url=settings.CLIENT_URL,
+                   token=verify_token)
 
-        plain_message = strip_tags(html_message)
-        send_email.delay(
-            subject='Register',
-            recipient_list=[user.email],
-            message=plain_message,
-            html_message=html_message
-        )
+        # html_message = render_to_string(
+        #     'emails/register.html', {'token': verify_token})
+        #
+        # plain_message = strip_tags(html_message)
+        # send_email.delay(
+        #     subject='Register',
+        #     recipient_list=[user.email],
+        #     message=plain_message,
+        #     html_message=html_message
+        # )
 
         return user
 
